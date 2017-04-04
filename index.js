ac52a8afb52c7d51c9c36cb84bc64a4b7322afb9
Chau Nguyen <chauforward@gmail.com>
2017-04-04 12:24:16 -0400

    forgot method

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ db.once('open', () => {
   console.log('connected to the db!')
 });
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+};
+
 app.use(morgan('dev'));
 // app.use(express.static('client'));
 app.use(allowCrossDomain);
